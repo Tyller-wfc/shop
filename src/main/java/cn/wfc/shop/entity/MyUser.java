208fed61149acd3327fdeffbf75d58c740005ee5
Tyller_wfc <wangfangchaozzu@163.com>
2020-11-10 21:54:40 +0800

    增加jrebel和lombok插件

--- src/main/java/cn/wfc/shop/entity/MyUser.java
+++ src/main/java/cn/wfc/shop/entity/MyUser.java
@@ -1,7 +1,9 @@
 package cn.wfc.shop.entity;
 
-import java.util.Date;
+import lombok.Data;
 
+import java.util.Date;
+@Data
 public class MyUser {
     private Integer id;
     private String username;
@@ -11,59 +13,4 @@ public class MyUser {
     private Date created;
     private Date updated;
 
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public Date getCreated() {
-        return created;
-    }
-
-    public void setCreated(Date created) {
-        this.created = created;
-    }
-
-    public Date getUpdated() {
-        return updated;
-    }
-
-    public void setUpdated(Date updated) {
-        this.updated = updated;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
